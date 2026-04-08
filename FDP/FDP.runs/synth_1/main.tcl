b7f89a8cd0148d4fa7747ffa9cc5c7465c22755f
Qiang Jiayuan <jiayuan.qiang127@gmail.com>
2026-04-08 20:22:57 +0800

    UI Update

--- FDP/FDP.runs/synth_1/main.tcl
+++ FDP/FDP.runs/synth_1/main.tcl
@@ -28,9 +28,19 @@ set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
 set_property ip_output_repo c:/Users/jiayu/Documents/GitHub/ee2026_project/FDP/FDP.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
-add_files C:/Users/jiayu/Documents/GitHub/ee2026_project/FDP/FDP.srcs/sources_1/imports/res/weights.coe
-add_files C:/Users/jiayu/Documents/GitHub/ee2026_project/FDP/FDP.srcs/sources_1/imports/res/00.coe
-read_verilog -library xil_defaultlib C:/Users/jiayu/Documents/GitHub/ee2026_project/FDP/FDP.srcs/sources_1/new/Top_Student.v
+read_verilog -library xil_defaultlib {
+  C:/Users/jiayu/Documents/GitHub/ee2026_project/FDP/FDP.srcs/sources_1/imports/Oled_Display.v
+  C:/Users/jiayu/Documents/GitHub/ee2026_project/FDP/FDP.srcs/sources_1/imports/clk_divider.v
+  C:/Users/jiayu/Documents/GitHub/ee2026_project/FDP/FDP.srcs/sources_1/new/cos_lut.v
+  C:/Users/jiayu/Documents/GitHub/ee2026_project/FDP/FDP.srcs/sources_1/new/debounce.v
+  C:/Users/jiayu/Documents/GitHub/ee2026_project/FDP/FDP.srcs/sources_1/new/game_state.v
+  C:/Users/jiayu/Documents/GitHub/ee2026_project/FDP/FDP.srcs/sources_1/new/lfsr16.v
+  C:/Users/jiayu/Documents/GitHub/ee2026_project/FDP/FDP.srcs/sources_1/new/render.v
+  C:/Users/jiayu/Documents/GitHub/ee2026_project/FDP/FDP.srcs/sources_1/new/sin_lut.v
+  C:/Users/jiayu/Documents/GitHub/ee2026_project/FDP/FDP.srcs/sources_1/new/ss_display.v
+  C:/Users/jiayu/Documents/GitHub/ee2026_project/FDP/FDP.srcs/sources_1/new/terrain_ram.v
+  C:/Users/jiayu/Documents/GitHub/ee2026_project/FDP/FDP.srcs/sources_1/new/main.v
+}
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
 # design are intentionally left as such for best results. Dcp files will be
@@ -45,12 +55,12 @@ set_property used_in_implementation false [get_files C:/Users/jiayu/Documents/Gi
 set_param ips.enableIPCacheLiteLoad 0
 close [open __synthesis_is_running__ w]
 
-synth_design -top Top_Student -part xc7a35tcpg236-1
+synth_design -top main -part xc7a35tcpg236-1
 
 
 # disable binary constraint mode for synth run checkpoints
 set_param constraints.enableBinaryConstraints false
-write_checkpoint -force -noxdef Top_Student.dcp
-create_report "synth_1_synth_report_utilization_0" "report_utilization -file Top_Student_utilization_synth.rpt -pb Top_Student_utilization_synth.pb"
+write_checkpoint -force -noxdef main.dcp
+create_report "synth_1_synth_report_utilization_0" "report_utilization -file main_utilization_synth.rpt -pb main_utilization_synth.pb"
 file delete __synthesis_is_running__
 close [open __synthesis_is_complete__ w]
